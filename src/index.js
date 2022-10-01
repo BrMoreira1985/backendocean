b33b96881f7862fd370b1f6dcc963f419de24076
Bruno Moreira <ok.computador@gmail.com>
2022-10-01 16:01:07 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { Pool } = require("pg");
+const pool = new Pool({ connectionString: process.env.BD_URL });
+const porta = "8080";
+
+app.use(express.json());
+app.use(cors);
+
+app.get("/clientes", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM clientes");
+    return res.status(200).send(rows);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+});
+
+app.post("/sessao", async (req, res) => {
+  const { nome_cliente } = req.body;
+  const { cpf } = req.body;
+  const { rua } = req.body;
+  const { numero } = req.body;
+  const { complemento } = req.body;
+  const { bairro } = req.body;
+  const { cep } = req.body;
+  const { cidade } = req.body;
+  const { estado } = req.body;
+
+  try {
+    const novaSessao = await pool.query(
+      "INSERT INTO clientes (nome_cliente, cpf, rua, numero, complemento, bairro, cep, cidade, estado) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+      [nome_cliente, cpf, rua, numero, complemento, bairro, cep, cidade, estado]
+    );
+    return res.status(200).send(novaSessao.rows);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+});
+
+app.listen(porta, () => {
+  console.log(`escutando na porta ${porta}`);
+});
